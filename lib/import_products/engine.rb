bae5110c2dd8a6fcce0b29fcca2a27f79047068e
Bounmy Stephane <stephanebounmy@gmail.com>
2012-03-25 16:06:06 +0200

    fixed require decorator + button html options

--- lib/import_products/engine.rb
+++ lib/import_products/engine.rb
@@ -5,8 +5,8 @@ module ImportProducts
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
       end
 
       Spree::UserMailer.send(:include, ImportProducts::UserMailerExt)
