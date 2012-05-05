8e14861818c0a1e7b7ba2cc75d2e7f3cfeafe52a
Bounmy Stephane <stephanebounmy@gmail.com>
2012-05-05 13:47:51 +0200

    fix override name to avoid conflict with promotions

--- app/overrides/import_admin_tabs.rb
+++ app/overrides/import_admin_tabs.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                     :name => "promo_admin_tabs",
+                     :name => "product_import_admin_tabs",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab(:product_imports) %>",
+                     :text => "<%= tab(:product_imports, :url => spree.admin_product_imports_path) %>",
                      :disabled => false)
